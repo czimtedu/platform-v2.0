19966daab317e71dd51bc23deec01a028c7785f2
chch <lufengc@163.com>
2016-09-19 23:40:13 +0800

    Modify some problems

---
+++ src/main/java/com/platform/modules/cms/action/CommentAction.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright &copy; <a href="http://www.zsteel.cc">zsteel</a> All rights reserved.
+ */
+
+package com.platform.modules.cms.action;
+
+import com.platform.framework.common.BaseAction;
+import com.platform.framework.common.Page;
+import com.platform.framework.util.StringUtils;
+import com.platform.modules.cms.bean.CmsComment;
+import com.platform.modules.cms.service.CommentService;
+import com.platform.modules.sys.bean.Param;
+import com.platform.modules.sys.utils.DictUtils;
+import com.platform.modules.sys.utils.UserUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * 站点
+ *
+ * @author lufengc
+ * @date 2016-01-15 09:56:22
+ */
+@Controller
+@RequestMapping(value = "${adminPath}/cms/comment")
+public class CommentAction extends BaseAction<CmsComment> {
+
+    @Autowired
+    private CommentService commentService;
+
+    /**
+     * 数据绑定
+     *
+     * @param id ID
+     * @return CmsComment
+     * @throws Exception
+     */
+    @Override
+    @ModelAttribute
+    public CmsComment get(@RequestParam(required = false) String id) throws Exception {
+        CmsComment cmsComment;
+        if (StringUtils.isNotEmpty(id)) {
+            cmsComment = commentService.get(id);
+        } else {
+            cmsComment = new CmsComment();
+        }
+        return cmsComment;
+    }
+
+    /**
+     * 列表
+     *
+     * @param model    Model
+     * @param object   object
+     * @param request  request
+     * @param response @return view
+     * @throws Exception
+     */
+    @Override
+    @RequestMapping(value = {"list", ""})
+    @RequiresPermissions("cms:comment:view")
+    public String list(Model model, CmsComment object, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Page<CmsComment> page = commentService.getPage(new Page<CmsComment>(request, response), object, null, "");
+        model.addAttribute("page", page);
+        return "modules/cms/commentList";
+    }
+
+    /**
+     * 编辑视图
+     *
+     * @param model  Model
+     * @param object object
+     * @return view
+     * @throws Exception
+     */
+    @Override
+    @RequestMapping(value = "form")
+    @RequiresPermissions("cms:comment:edit")
+    public String form(Model model, CmsComment object) throws Exception {
+        return "modules/cms/commentForm";
+    }
+
+    /**
+     * 保存
+     *
+     * @param model  Model
+     * @param object object
+     * @return view
+     * @throws Exception
+     */
+    @Override
+    @RequestMapping(value = "save")
+    @RequiresPermissions("cms:comment:edit")
+    protected String save(Model model, CmsComment object, RedirectAttributes redirectAttributes) throws Exception {
+        if (beanValidator(redirectAttributes, object)) {
+            if (object.getAuditUserId() == null) {
+                object.setAuditUserId(UserUtils.getUserId());
+                object.setAuditDate(new Date());
+            }
+            commentService.save(object);
+            addMessage(redirectAttributes, DictUtils.getDictLabel(object.getDelFlag(), "cms_status", "保存")
+                    + "评论'" + StringUtils.abbr(StringUtils.replaceHtml(object.getContent()), 50) + "'成功");
+        }
+        addMessage(redirectAttributes, "保存成功");
+        return "redirect:" + adminPath + "/cms/comment/list?repage";
+    }
+
+    /**
+     * 删除
+     *
+     * @param model  Model
+     * @param object object
+     * @return view
+     * @throws Exception
+     */
+    @Override
+    @RequestMapping(value = "delete")
+    @RequiresPermissions("cms:comment:edit")
+    public String delete(Model model, CmsComment object, Param param, RedirectAttributes redirectAttributes) throws Exception {
+        commentService.delete(object.getId());
+        addMessage(redirectAttributes, "删除成功");
+        return "redirect:" + adminPath + "/cms/comment/list?repage";
+    }
+
+}
--- src/main/webapp/WEB-INF/views/modules/cms/siteForm.jsp
+++ src/main/webapp/WEB-INF/views/modules/cms/siteForm.jsp
@@ -26,29 +26,25 @@
 	</script>
 </head>
 <body>
-	<ul class="nav nav-tabs">
-		<li><a href="${ctx}/cms/site/">站点列表</a></li>
-		<li class="active"><a href="${ctx}/cms/site/form?id=${site.id}">站点<shiro:hasPermission name="cms:site:edit">${not empty site.id?'修改':'添加'}</shiro:hasPermission><shiro:lacksPermission name="cms:site:edit">查看</shiro:lacksPermission></a></li>
-	</ul><br/>
-	<form:form id="inputForm" modelAttribute="site" action="${ctx}/cms/site/save" method="post" class="form-horizontal">
+	<form:form id="inputForm" modelAttribute="cmsSite" action="${ctx}/cms/site/save" method="post" class="form-horizontal">
 		<form:hidden path="id"/>
 		<sys:message content="${message}"/>
 		<div class="control-group">
 			<label class="control-label">站点名称:</label>
 			<div class="controls">
-				<form:input path="name" htmlEscape="false" maxlength="200" class="input-xlarge required"/>
+				<form:input path="name" htmlEscape="false" maxlength="200" cssClass="form-control required"/>
 			</div>
 		</div>
 		<div class="control-group">
 			<label class="control-label">站点标题:</label>
 			<div class="controls">
-				<form:input path="title" htmlEscape="false" maxlength="200" class="input-xlarge required"/>
+				<form:input path="title" htmlEscape="false" maxlength="200" cssClass="form-control required"/>
 			</div>
 		</div>
 		<div class="control-group">
 			<label class="control-label">站点Logo:</label>
 			<div class="controls">
-				<form:hidden path="logo" htmlEscape="false" maxlength="255" class="input-xlarge"/>
+				<form:hidden path="logo" htmlEscape="false" maxlength="255" cssClass="form-control"/>
 				<sys:ckfinder input="logo" type="images" uploadPath="/cms/site"/>
 				<span class="help-inline">建议Logo大小：1000 × 145（像素）</span>
 			</div>
@@ -56,20 +52,20 @@
 		<div class="control-group">
 			<label class="control-label">描述:</label>
 			<div class="controls">
-				<form:textarea path="description" htmlEscape="false" rows="4" maxlength="200" class="input-xxlarge"/>
+				<form:textarea path="description" htmlEscape="false" rows="4" maxlength="200" cssClass="form-control"/>
 			</div>
 		</div>
 		<div class="control-group">
 			<label class="control-label">关键字:</label>
 			<div class="controls">
-				<form:input path="keywords" htmlEscape="false" maxlength="200"/>
+				<form:input path="keywords" htmlEscape="false" maxlength="200" cssClass="form-control"/>
 				<span class="help-inline">填写描述及关键字，有助于搜索引擎优化</span>
 			</div>
 		</div>
 		<div class="control-group">
 			<label class="control-label">默认主题:</label>
 			<div class="controls">
-				<form:select path="theme" class="input-medium">
+				<form:select path="theme" cssClass="form-control">
 					<form:options items="${fns:getDictList('cms_theme')}" itemLabel="label" itemValue="value" htmlEscape="false"/>
 				</form:select>
 			</div>
@@ -77,19 +73,15 @@
 		<div class="control-group">
 			<label class="control-label">版权信息:</label>
 			<div class="controls">
-				<form:textarea id="copyright" htmlEscape="true" path="copyright" rows="4" maxlength="200" class="input-xxlarge"/>
+				<form:textarea id="copyright" htmlEscape="true" path="copyright" rows="4" maxlength="200" cssClass="form-control"/>
 				<sys:ckeditor replace="copyright" uploadPath="/cms/site" />
 			</div>
 		</div>
 		<div class="control-group">
 			<label class="control-label">自定义首页视图:</label>
 			<div class="controls">
-				<form:input path="customIndexView" htmlEscape="false" maxlength="200"/>
+				<form:input path="customIndexView" htmlEscape="false" maxlength="200" cssClass="form-control"/>
 			</div>
-		</div>
-		<div class="form-actions">
-			<shiro:hasPermission name="cms:site:edit"><input id="btnSubmit" class="btn btn-primary" type="submit" value="保 存"/>&nbsp;</shiro:hasPermission>
-			<input id="btnCancel" class="btn" type="button" value="返 回" onclick="history.go(-1)"/>
 		</div>
 	</form:form>
 </body>
